854b25dc0640f231e1aeb2b90fca2c97aa5a4ea2
Botir0418 <66214373+Botir0418@users.noreply.github.com>
2021-10-23 19:09:57 +0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+//JSX ishlatilmaganda
+const myElement = React.createElement('h1',{ style: {color : "black"}}, "salom aka")
+ReactDOM.render(myElement,document.getElementById('h1'));
+
+//JSX ishlatilganda
+const myElement1 = <h3 style={{color: "blue"}}>qalay</h3>
+ReactDOM.render(myElement1,document.getElementById('h3'));
+
+
+const myElement3 = <h2>2 ga 2ni ko'paytirsak {2*2} </h2>
+ReactDOM.render(myElement3,document.getElementById('h5'));
+
+const name = "Botir"
+const myel= <h1>salom,{name}</h1>
+ReactDOM.render(myel,document.getElementById('h6'));
+
+  
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+);
+
+
+
+
+reportWebVitals();
